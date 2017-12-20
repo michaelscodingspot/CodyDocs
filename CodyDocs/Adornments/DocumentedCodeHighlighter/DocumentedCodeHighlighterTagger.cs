42666859a197fe524b25322b23e1f0d9449c3168
Michael Shpilt <michaels@codevalue.net>
2017-12-20 17:57:10 +0200

    Adding "DocumentedCodeHighlighter" to project. Now highlighting random span of text

---
+++ CodyDocs/Adornments/DocumentedCodeHighlighter/DocumentedCodeHighlighterTagger.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodyDocs.Adornments.DocumentedCodeHighlighter
+{
+    public class DocumentedCodeHighlighterTag : TextMarkerTag
+    {
+        public DocumentedCodeHighlighterTag() : base("blue") { }
+    }
+
+    class DocumentedCodeHighlighterTagger : ITagger<DocumentedCodeHighlighterTag>
+    {
+        private ITextView _textView;
+        private ITextBuffer _buffer;
+
+        public DocumentedCodeHighlighterTagger(ITextView textView, ITextBuffer buffer)
+        {
+            _textView = textView;
+            _buffer = buffer;
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+
+        public IEnumerable<ITagSpan<DocumentedCodeHighlighterTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            var tag1 = new TagSpan<DocumentedCodeHighlighterTag>(new SnapshotSpan(_buffer.CurrentSnapshot, new Span(30, 40)), new DocumentedCodeHighlighterTag());
+
+            return new List<ITagSpan<DocumentedCodeHighlighterTag>>() { tag1 };
+        }
+    }
+}
