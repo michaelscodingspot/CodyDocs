22bcb4fa43421cc720b1e5dee0be5dcb3c181612
Michael Shpilt <michaels@codevalue.net>
2018-05-04 19:04:44 +0300

    Glyph margin classes

---
+++ CodyDocs/EditorUI/Glyph/CommentGlyphFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CodyDocs.EditorUI.Glyph
+{
+    /// <summary>
+    /// This class implements IGlyphFactory, which provides the visual
+    /// element that will appear in the glyph margin.
+    /// </summary>
+    internal class CommentGlyphFactory : IGlyphFactory
+    {
+
+        public UIElement GenerateGlyph(IWpfTextViewLine line, IGlyphTag tag)
+        {
+            var lineHeight = line.Height;
+            var grid = new System.Windows.Controls.Grid()
+            {
+                Width = lineHeight,
+                Height = lineHeight
+            };
+            grid.Children.Add(new Rectangle()
+            {
+                Fill = Brushes.YellowGreen,
+                Width = lineHeight / 3,
+                Height = lineHeight,
+                HorizontalAlignment = HorizontalAlignment.Right
+            });
+            return grid;
+        }
+
+    }
+}
---
+++ CodyDocs/EditorUI/Glyph/CommentGlyphFactoryProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace CodyDocs.EditorUI.Glyph
+{
+    [Export(typeof(IGlyphFactoryProvider))]
+    [Name("CommentGlyph")]
+    [Order(Before = "VsTextMarker")]
+    [ContentType("code")]
+    [TagType(typeof(CommentTag))]
+    internal sealed class CommentGlyphFactoryProvider : IGlyphFactoryProvider
+    {
+        public IGlyphFactory GetGlyphFactory(IWpfTextView view, IWpfTextViewMargin margin)
+        {
+            return new CommentGlyphFactory();
+        }
+
+    }
+}
